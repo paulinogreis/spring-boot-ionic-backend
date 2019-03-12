c97335b71ba718cac3f9c28d5014b05d2db4f4cd
paulino reis <paulinogreis@gmail.com>
2019-03-12 16:11:06 -0300

    Put Delete Get para cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -34,7 +34,7 @@ public class ClienteService {
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
-		return repo.save(obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -55,7 +55,7 @@ public class ClienteService {
 	}
 
 	public Cliente fromDTO(ClienteDTO objDto) {
-		throw new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 
 	public void updateData(Cliente newObj, Cliente obj) {
