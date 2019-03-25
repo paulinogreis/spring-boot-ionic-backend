e8020f65d2c9f7347ac6ec736bbcb16e2b2544b6
paulino reis <paulinogreis@gmail.com>
2019-03-25 12:22:45 -0300

    SubTotal e Total do pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -98,4 +98,8 @@ public class ItemPedido implements Serializable {
 	public ItemPedido() {
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 }
--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -126,4 +126,11 @@ public class Pedido implements Serializable {
 	public Pedido() {
 	}
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
 }
