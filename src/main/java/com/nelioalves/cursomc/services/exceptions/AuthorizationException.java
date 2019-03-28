94b347bf0672cd190db2431f65a8befbe0c41381
paulino reis <paulinogreis@gmail.com>
2019-03-28 20:49:09 -0300

    Restricao de conteudo, cliente so recupera ele mesmo

---
+++ src/main/java/com/nelioalves/cursomc/services/exceptions/AuthorizationException.java
@@ -0,0 +1,10 @@
+package com.nelioalves.cursomc.services.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AuthorizationException(String msg) {
+		super(msg);
+	}
+}
--- src/main/java/com/nelioalves/cursomc/services/exceptions/DataIntegrityException.java
+++ src/main/java/com/nelioalves/cursomc/services/exceptions/DataIntegrityException.java
@@ -4,7 +4,7 @@ public class DataIntegrityException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public DataIntegrityException(String msg) {
-		super(msg);
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
 	}
 }
