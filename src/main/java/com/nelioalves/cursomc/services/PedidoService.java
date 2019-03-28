16623c67ee0789fb9890670fdc84bcc06f3c4cb4
paulino reis <paulinogreis@gmail.com>
2019-03-27 21:00:07 -0300

    Implementando string de pedido

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -29,6 +29,8 @@ public class PedidoService {
 	private ProdutoRepository produtoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -39,6 +41,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -50,10 +53,12 @@ public class PedidoService {
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			Optional<Produto> prod = produtoRepository.findById(ip.getProduto().getId());
+			ip.setProduto(prod.get());
 			ip.setPreco(prod.get().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
